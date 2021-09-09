8222d77856d2912c8b1360ef2b2b39eec1682010
Eliana-Abate <eliana.abate@outlook.it>
2021-09-09 15:54:14 +0200

    Header completed + added Scss and basic other components file

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
+    <Header/>
+    
    
   </div>
 </template>
 
+
 <script>
+import Header from '@/components/Header.vue';
 
 
 export default {
   name: 'App',
   components: {
+    Header,
     
   }
 }
 </script>
 
 <style lang="scss">
+  @import "@/assets/scss/style.scss";
 
 </style>
